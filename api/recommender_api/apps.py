340c9cdcef52ad831bf3fec086212941a930bf84
Mitchell Bird <mitchell.bird@live.co.uk>
2026-01-04 18:14:22 +0000

    moved data cache to entry point script, causes issues when coupled with django

--- api/recommender_api/apps.py
+++ api/recommender_api/apps.py
@@ -1,11 +1,1 @@
 from django.apps import AppConfig
-
-
-class MediaRSConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "api.recommender_api"
-    label = "recommender_api_app"
-
-    def ready(self):
-        from media_rs.utils.movies.movie_data_cache import get_movie_data_cache
-        get_movie_data_cache()
