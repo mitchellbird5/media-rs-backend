683897f66531358ffc763d0acef9137168b77abc
Mitchell Bird <mitchell.bird@live.co.uk>
2026-01-04 03:20:11 +0000

    added docker stuff

--- api/recommender_api/services/collab_services.py
+++ api/recommender_api/services/collab_services.py
@@ -4,18 +4,20 @@ from media_rs.serving.recommender.build.build_collab_model import (
     get_item_cf_model,
     get_user_cf_model
 )
-from media_rs.utils.movies.movie_data_cache import MOVIE_DATA_CACHE
+from media_rs.utils.movies.movie_data_cache import get_movie_data_cache
 
 from typing import List, Dict
+
+cache = get_movie_data_cache()
 
 def get_item_cf_recommendations(
     movie_title: str,
     top_n: int = 10
 ) -> List[ContentSimilarity]:
     
-    item_idx = ItemIndex("media_rs/serving/artifacts/item_index.pkl")
+    item_idx = ItemIndex(cache.get("item_index.pkl"))
     
-    rs = get_item_cf_model(MOVIE_DATA_CACHE)
+    rs = get_item_cf_model(cache)
     
     recommendations = rs.recommend(item_idx.title_to_idx[movie_title], top_n)
     return [item_idx.idx_to_title[r[0]] for r in recommendations]
@@ -26,7 +28,7 @@ def get_user_cf_recommendations(
     k_similar_users: int
 ) -> List[ContentSimilarity]:
     
-    item_idx = ItemIndex("media_rs/serving/artifacts/item_index.pkl")
+    item_idx = ItemIndex(cache.get("item_index.pkl"))
     
     index_ratings = {
         item_idx.title_to_idx[title]: score
@@ -34,7 +36,7 @@ def get_user_cf_recommendations(
         if title in item_idx.title_to_idx
     }
     
-    rs = get_user_cf_model(MOVIE_DATA_CACHE)
+    rs = get_user_cf_model(cache)
     
     recommendations = rs.recommend(
         index_ratings, 
