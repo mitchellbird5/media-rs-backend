ec4ef85c7e8d0b6e654986c4475ffb57cbfd7a5f
Mitchell Bird <mitchell.bird@live.co.uk>
2026-01-21 03:08:28 +0000

    removed unused models

--- api/recommender_api/models.py
+++ api/recommender_api/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Movie(models.Model):
-    title = models.CharField(max_length=255)
-    genre = models.CharField(max_length=100)
-    release_year = models.IntegerField()
-    
-    def __str__(self):
-        return self.title
